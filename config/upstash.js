48db3bb890aea8549c730878d48b1266460fa1da
2early4coffee <Walteniumroy@gmail.com>
2026-07-16 00:12:11 +0300

    refactor workflow client import and update subscription fetching logic

--- config/upstash.js
+++ config/upstash.js
@@ -1,8 +1,6 @@
-import { Client as WorkflowClient } from '@upstash/qstash';
+import { Client } from '@upstash/workflow';
+import { QSTASH_TOKEN } from './env.js';
 
-import { QSTASH_TOKEN, QSTASH_URL } from './env.js';
-
-export const workflowClient = new WorkflowClient({
-    baseUrl: QSTASH_URL,
+export const workflowClient = new Client({
     token: QSTASH_TOKEN,
 });
